8b3da8a7f62712e509d61754b22159858b6da295
web-dot <fedev.sudipto@gmail.com>
2021-10-28 01:18:40 +0530

    emp-reg-update01

--- public/employee-reg/script.js
+++ public/employee-reg/script.js
@@ -1,113 +1,66 @@
-var selectedRow = null
 
-let empArray = localStorage.getItem('emps') ? JSON.parse(localStorage.getItem('emps')):[];
+/*
+Steps to Program
+main->
+1]onsubmit
+    1.1]display
+    1.2]put in LS
+2]display from LS
+sub->
+    1.1.1]create empty arr
+    1.1.2]create object
+    1.1.3]put in arr
+    1.1.4]iterate arr and display(function)
 
-function onFormSubmit() {
-    if (validate()) {
-        var Employee = readFormData();
-        console.log(Employee);
-        if (selectedRow == null){
-            insertNewRecord(Employee);
-            empArray.push(Employee);
-            localStorage.setItem('emps', JSON.stringify(empArray));
-            console.log(empArray);
-        }
-        else
-            updateRecord(Employee);
-        resetForm();
+*/
+
+//1.1.1]if 'employees' exist in LS, get 'employees' else create empty arr 
+var empArr = localStorage.getItem('employees') ? JSON.parse(localStorage.getItem('employees')):[];
+
+//grab table using id
+const nameTable = document.getElementById('nameTable');
+//grab the form
+const form = document.querySelector("form");
+//2]display from LS
+display(empArr);
+
+form.addEventListener('submit', function onSubmit(e){
+    e.preventDefault();
+    //getting Employee object
+    var Employee = getObject();
+    console.log(Employee);
+    //1.1.3]put in arr
+    empArr.push(Employee);
+    console.log(empArr);
+    display(empArr);
+    //1.2]put in LS
+    localStorage.setItem('employees', JSON.stringify(empArr));
+});
+
+//1.1.2]function to create object
+function getObject(){
+    var Employee = {};
+    Employee["name"] = document.getElementById('personName').value;
+    Employee["email"] = document.getElementById('personEmail').value;
+    Employee["salary"] = document.getElementById('personSalary').value;
+    return  Employee;
+}
+
+function display(arr){
+    let content = nameTable.innerHTML;
+    for(var i=0; i<arr.length; i++){
+        //let content = nameTable.innerHTML;
+        content = content + '<tr><td>'+ arr[i]["name"]+'</td><td>'+ arr[i]["email"]+'</td><td>'+ arr[i]["salary"]+'</td><td>' + '<button class="upBt">update</button><button class="delBt">delete</button></tr>';
+        nameTable.innerHTML = content;
+        personName.value ='';
+        personEmail.value ='';
+        personSalary.value = '';    
+    
     }
 }
 
 
 
-function readFormData() {
-    var Employee = {};  //creating empty Employee object literal
-    //setting Employee values using bracket notation 
-    Employee["fullName"] = document.getElementById("fullName").value;
-    Employee["email"] = document.getElementById("email").value;
-    Employee["salary"] = document.getElementById("salary").value;
-    return Employee;
-}
-
-function insertNewRecord(data) {
-
-    //getting hold of table body
-    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
-
-    //inserting a new row in table
-    var newRow = table.insertRow(table.length);
-
-    //inserting new cell in row
-    cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.fullName;
-
-    //inserting new cell row
-    cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.email;
-
-    //inserting new cell row
-    cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.salary;
-
-    //inserting new cell row
-    cell4 = newRow.insertCell(3);
-    cell4 = newRow.insertCell(4);
-    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
-                       <a onClick="onDelete(this)">Delete</a>`;
-}
-
-function resetForm() {
-    document.getElementById("fullName").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("salary").value = "";
-    selectedRow = null;
-}
-
-function onEdit(td) {
-    selectedRow = td.parentElement.parentElement;
-    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("email").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
-}
-function updateRecord(Employee) {
-    selectedRow.cells[0].innerHTML = Employee.fullName;
-    selectedRow.cells[1].innerHTML = Employee.email;
-    selectedRow.cells[2].innerHTML = Employee.salary;
-}
-
-
-  
 
 
 
-function onDelete(td) {
-    if (confirm('Are you sure to delete this record ?')) {
-        row = td.parentElement.parentElement;
-        document.getElementById("employeeList").deleteRow(row.rowIndex);
-        console.log(row.firstChild.innerHTML);
-        var empName = row.firstChild.innerHTML;
-        for(var i=0; i<empArray.length; i++){
-            if(empArray[i]["fullName"] === empName){
-                empArray.splice(i, 1);
-            }
-        }
-        localStorage.setItem('emps', JSON.stringify(empArray));
-        console.log(empArray);  
-        resetForm();
-    }
-}
-
-
-
-function validate() {
-    isValid = true;
-    if (document.getElementById("fullName").value == "") {
-        isValid = false;
-        document.getElementById("fullNameValidationError").classList.remove("hide");
-    } else {
-        isValid = true;
-        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
-            document.getElementById("fullNameValidationError").classList.add("hide");
-    }
-    return isValid;
-}
