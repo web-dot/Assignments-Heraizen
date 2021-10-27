a4a8f7798e93e1a286d9431599b28773f1d33f06
web-dot <fedev.sudipto@gmail.com>
2021-10-28 02:44:41 +0530

    emp-reg-updt02-LS-add-del

--- public/employee-reg/script.js
+++ public/employee-reg/script.js
@@ -6,6 +6,9 @@ main->
     1.1]display
     1.2]put in LS
 2]display from LS
+3]delete
+    3.1]delete from array
+    3.2]replace in LS
 sub->
     1.1.1]create empty arr
     1.1.2]create object
@@ -21,8 +24,10 @@ var empArr = localStorage.getItem('employees') ? JSON.parse(localStorage.getItem
 const nameTable = document.getElementById('nameTable');
 //grab the form
 const form = document.querySelector("form");
+//grab the delete btn
+const delBtn = document.querySelector(".delBt");
 //2]display from LS
-display(empArr);
+displayLS(empArr);
 
 form.addEventListener('submit', function onSubmit(e){
     e.preventDefault();
@@ -32,7 +37,7 @@ form.addEventListener('submit', function onSubmit(e){
     //1.1.3]put in arr
     empArr.push(Employee);
     console.log(empArr);
-    display(empArr);
+    display();
     //1.2]put in LS
     localStorage.setItem('employees', JSON.stringify(empArr));
 });
@@ -46,21 +51,40 @@ function getObject(){
     return  Employee;
 }
 
-function display(arr){
-    let content = nameTable.innerHTML;
-    for(var i=0; i<arr.length; i++){
-        //let content = nameTable.innerHTML;
-        content = content + '<tr><td>'+ arr[i]["name"]+'</td><td>'+ arr[i]["email"]+'</td><td>'+ arr[i]["salary"]+'</td><td>' + '<button class="upBt">update</button><button class="delBt">delete</button></tr>';
+function display(){
+        var Employee = getObject();
+        let content = nameTable.innerHTML;
+        content = content + '<tr><td>'+ Employee["name"]+'</td><td>'+ Employee["email"]+'</td><td>'+ Employee["salary"]+'</td><td>' + '<button class="upBt">update</button><button class="delBt">delete</button></tr>';
         nameTable.innerHTML = content;
         personName.value ='';
         personEmail.value ='';
         personSalary.value = '';    
-    
+}
+
+function displayLS(arr){
+    let content = nameTable.innerHTML;
+    for(var i=0; i<arr.length; i++){
+        content = content + '<tr><td>'+ arr[i]["name"]+'</td><td>'+ arr[i]["email"]+'</td><td>'+ arr[i]["salary"]+'</td><td>' + '<button class="upBt">update</button><button class="delBt">delete</button></tr>';
+        nameTable.innerHTML = content;
     }
 }
 
 
-
-
+var setEventListener = function(){
+    nameTable.addEventListener('click', function(e){
+        var elementClicked = e.target;
+        var row = elementClicked.parentNode.parentNode;
+        nameTable.deleteRow(row.rowIndex);
+        var empName = row.firstChild.innerHTML;
+        for(var i=0; i<empArr.length; i++){
+        if(empArr[i]["name"] === empName){
+            empArr.splice(i, 1);
+            }
+        }
+        localStorage.setItem('employees', JSON.stringify(empArr));
+        console.log(empArr);
+    });
+}
+setEventListener();
 
 
